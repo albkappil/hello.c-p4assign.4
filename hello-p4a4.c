5d62ead23584c742aef7826f35cbb23f3cec0aca
albkappil <50112186+albkappil@users.noreply.github.com>
2019-04-29 10:43:33 -0500

    Add files via upload

---
+++ hello-p4a4.c
@@ -0,0 +1,8 @@
+// assignment 4 part 4
+#include <stdio.h>
+#include <stdlib.h>   
+int main()
+{
+	printf("Hello World\n");   // waw hello Richard and ok
+ 	exit(0);         // end of the program
+}
